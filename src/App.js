72212936f0005a5c75baaff4a03b56cbb62d35d5
Geek Code <rovafiti@email.com>
2024-01-11 00:44:41 +0300

    Ajout du composent Skills et Work dans mon Portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import About from "./components/pages/About.jsx";
 import Home from "./components/pages/Home.jsx";
 import Skills from "./components/pages/Skills.jsx";
+import Work from "./components/pages/Work.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   );
 }
--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -11,12 +11,12 @@ import Mysql from '../../assets/icons8-mysql-144.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-pink-600 inline'>Skills</p>
-                <p className='py-4'>// These are a Technologies I've worked with</p>
+                <p className='py-6'>// These are a Technologies I've worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
