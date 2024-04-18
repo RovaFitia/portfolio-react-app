c8ff62e319d84179a1952d73831fd7cf96ea6362
rova fitia <rovafiti@gmail.com>
2024-04-18 23:32:15 +0300

    create and init project Portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Anek_Telugu } from "next/font/google";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { cn } from "@/lib/utils";
+
+const AnekTelugu = Anek_Telugu({ 
+  subsets: ["latin"] ,
+  variable: "--font-caption",
+});
+
+export const metadata: Metadata = {
+  title: "Appolo . Software Engineer",
+  description: "Portfolio in react, tailwind, Next js and Typescript",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-forground")}>{children}</body>
+    </html>
+  );
+}
