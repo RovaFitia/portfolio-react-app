0ca976ae574805ca79a13ada26746d5842ed92f3
rova fitia <rovafiti@gmail.com>
2024-07-11 19:39:48 +0300

    Feat: change Title in header and add new Work

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,57 +1,70 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
-import {motion} from 'framer-motion'
-import { Link } from 'react-scroll';
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Home = () => {
-
-    const elements = [
-        { type: "paragraph", text: "Hi, my name is", className: "text-pink-600" },
-        { type: "title", text: "Rova Fitia", className: "text-4xl sm:text-7xl font-bold text-[#ccd6f6]" },
-        { type: "subtitle", text: "I'm a Full Stack Developper", className: "text-4xl sm:text-7xl font-bold text-[#8892b0]" },
-        { type: "paragraph", text: "Self-taught passionate about web development, I stand out for my speed of learning and my determination to continually evolve through passion.", className: "text-[#8892b0] py-4 max-w-[700px]" },
-    ];
-
+  const elements = [
+    { type: "paragraph", text: "Hi, my name is", className: "text-pink-600" },
+    {
+      type: "title",
+      text: "Rova Fitia",
+      className: "text-4xl sm:text-7xl font-bold text-[#ccd6f6]",
+    },
+    {
+      type: "subtitle1",
+      text: "I'm a Web",
+      className: "text-4xl sm:text-7xl font-bold text-[#8892b0]",
+    },
+    {
+      type: "subtitle2",
+      text: "Developper",
+      className: "text-4xl sm:text-7xl font-bold text-[#8892b0]",
+    },
+    {
+      type: "paragraph",
+      text: "Self-taught passionate about web development, I stand out for my speed of learning and my determination to continually evolve through passion.",
+      className: "text-[#8892b0] py-4 max-w-[700px]",
+    },
+  ];
 
   return (
-    <div name="home" className='w-full h-screen bg-[#0a192f]'>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto h-full px-8 flex flex-col justify-center">
+        {elements.map((element, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.5, delay: index * 1.5 }}
+            className={element.className}
+          >
+            {element.type === "title" && <h1>{element.text}</h1>}
+            {element.type === "subtitle1" && <h2>{element.text}</h2>}
+            {element.type === "subtitle2" && <h2>{element.text}</h2>}
+            {element.type === "paragraph" && <p>{element.text}</p>}
+          </motion.div>
+        ))}
 
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto h-full px-8 flex flex-col justify-center'>
-            {elements.map((element, index) => (
-            <motion.div
-                key={index}
-                initial={{ opacity: 0, x: -100 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1.5, delay: index * 1.5 }}
-                className={element.className}
+        <div>
+          <Link to="about" smooth={true} duration={500} className="w-auto">
+            <motion.button
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.5, delay: elements.length * 1.45 }}
+              className="text-white group border-2 py-3 px-6 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"
             >
-                {element.type === 'title' && <h1>{element.text}</h1>}
-                {element.type === 'subtitle' && <h2>{element.text}</h2>}
-                {element.type === 'paragraph' && <p>{element.text}</p>}
-            </motion.div>
-            ))}
-            
-            <div>
-                <Link to="about" smooth={true} duration={500} className='w-auto'>
-                    <motion.button
-                        initial={{ opacity: 0, x: -100 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 1.5, delay: elements.length * 1.45 }}
-                        className='text-white group border-2 py-3 px-6 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
-                    >
-                        
-                            View Work 
-                            <span className='group-hover:rotate-90 duration-300'>
-                            <HiArrowNarrowRight className="ml-3"/>
-                            </span>
-                    </motion.button>
-                </Link>
-            </div>
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </motion.button>
+          </Link>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -1,91 +1,124 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React from "react" ;
-import Image1 from "../../assets/work/Igoblog.png" ;
-import Image2 from "../../assets/work/Nir-info.png" ;
-import Image3 from "../../assets/work/Leslandines.png" ;
-import Image4 from "../../assets/work/myexpat.png" ;
-import Image5 from "../../assets/work/Investissement-locatif.png" ;
-import Image6 from "../../assets/work/nousgerons.png" ;
-import { motion, useAnimation } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import React from "react";
+import Image1 from "../../assets/work/Igoblog.png";
+import Image2 from "../../assets/work/Nir-info.png";
+import Image3 from "../../assets/work/Leslandines.png";
+import Image4 from "../../assets/work/myexpat.png";
+import Image5 from "../../assets/work/Investissement-locatif.png";
+import Image6 from "../../assets/work/nousgerons.png";
+import Image7 from "../../assets/work/beetravel.png";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const datas = [
-    {image: Image1, title: 'Blog du site Igoblog', url: 'https://igoblog.fr'} ,
-    {image: Image2, title: 'Blog de l\'entreprise IT Nir\'info', url: 'https://nir-info.mg'} ,
-    {image: Image3, title: 'Blog de l\'entreprise Leslandines', url:'https://leslandines.com'} ,
-    {image: Image4, title: 'Blog de l\'entreprise My expat', url:'https://myexpat.fr'} ,
-    {image: Image5, title: 'Intégration du dashboard du site Investissement locatif', url:'https://investissement-locatif.com/'} ,
-    {image: Image6, title: 'Intégration du site Nousgerons', url:'https://nousgerons.com/'} ,
-] ;
+  { image: Image1, title: "Blog du site Igoblog", url: "https://igoblog.fr" },
+  {
+    image: Image2,
+    title: "Blog de l'entreprise IT Nir'info",
+    url: "https://nir-info.mg",
+  },
+  {
+    image: Image3,
+    title: "Blog de l'entreprise Leslandines",
+    url: "https://leslandines.com",
+  },
+  {
+    image: Image4,
+    title: "Blog de l'entreprise My expat",
+    url: "https://myexpat.fr",
+  },
+  {
+    image: Image5,
+    title: "Intégration du dashboard du site Investissement locatif",
+    url: "https://investissement-locatif.com/",
+  },
+  {
+    image: Image6,
+    title: "Intégration du site Nousgerons",
+    url: "https://nousgerons.com/",
+  },
+  {
+    image: Image7,
+    title: "Création du site Beetravel",
+    url: "https://github.com/RovaFitia/beetravel",
+  },
+];
 
 const variants = {
-    hidden: { opacity: 0, y: 80 },
-    visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 80 },
+  visible: { opacity: 1, y: 0 },
 };
 const Work = () => {
-    const controls = useAnimation();
-    const elementRefs = useRef([]);
-    
-    useEffect(() => {
-        const handleScroll = () => {
-            elementRefs.current.forEach((elementRef, index) => {
-                const elementTop = elementRef.offsetTop;
-                const scrollPosition = window.scrollY + window.innerHeight;
+  const controls = useAnimation();
+  const elementRefs = useRef([]);
 
-                if (scrollPosition > elementTop) {
-                    controls.start('visible');
-                }
-            });
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      elementRefs.current.forEach((elementRef, index) => {
+        const elementTop = elementRef.offsetTop;
+        const scrollPosition = window.scrollY + window.innerHeight;
 
-        window.addEventListener('scroll', handleScroll);
+        if (scrollPosition > elementTop) {
+          controls.start("visible");
+        }
+      });
+    };
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [controls]);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls]);
 
   return (
     <div name="work" className="w-full md:h-screen bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-2">
-          <p className="text-4xl font-bold border-b-4 border-pink-600 inline">Work</p>
+          <p className="text-4xl font-bold border-b-4 border-pink-600 inline">
+            Work
+          </p>
           <p className="py-6">// Check out some of my recent work</p>
         </div>
-      {/* Container */}
+        {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {/* Grid-item */}
+          {/* Grid-item */}
 
-            {datas.map((item, index) => (
-                <motion.div 
-                    key={index}
-                    ref={(ref) => (elementRefs.current[index] = ref)}
-                    style={{ backgroundImage: `url(${item.image})`, backgroundSize: 'cover', backgroundPosition: 'top center'}}  
-                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-                    initial='hidden'
-                    animate={controls}
-                    variants={variants}
-                    transition={{ delay: index * 0.5 }}
-                >
-                
-                    <div className='flex flex-col items-center justify-center gap-1 opacity-0 group-hover:opacity-100'>
-                        <span className='text-[18px] text-wrap block w-[200px] font-bold text-white tracking-wider text-center'>
-                            {item.title}
-                        </span>
-                        <div className='text-center'>
-                            <a href={item.url} target="_bank">
-                                <button className='text-center rounded-lg px-3 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>Demo</button>
-                            </a>
-                            <a href="https://github.com/RovaFitia" target="_bank">
-                                <button className='text-center rounded-lg px-3 py-3 m-2 bg-white text-gray-700 font-bold text-sm'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </motion.div>
-            ))}
-
+          {datas.map((item, index) => (
+            <motion.div
+              key={index}
+              ref={(ref) => (elementRefs.current[index] = ref)}
+              style={{
+                backgroundImage: `url(${item.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "top center",
+              }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+              initial="hidden"
+              animate={controls}
+              variants={variants}
+              transition={{ delay: index * 0.5 }}
+            >
+              <div className="flex flex-col items-center justify-center gap-1 opacity-0 group-hover:opacity-100">
+                <span className="text-[18px] text-wrap block w-[200px] font-bold text-white tracking-wider text-center">
+                  {item.title}
+                </span>
+                <div className="text-center">
+                  <a href={item.url} target="_bank">
+                    <button className="text-center rounded-lg px-3 py-3 m-2 bg-white text-gray-700 font-bold text-sm">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/RovaFitia" target="_bank">
+                    <button className="text-center rounded-lg px-3 py-3 m-2 bg-white text-gray-700 font-bold text-sm">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </motion.div>
+          ))}
         </div>
-
       </div>
     </div>
   );
